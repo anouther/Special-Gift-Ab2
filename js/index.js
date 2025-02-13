9b8ba4b9f3def722e2e21ab2fd9cb36947130b65
anouther <155570107+anouther@users.noreply.github.com>
2025-02-13 09:25:24 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const title = document.querySelector('.title')
-const text = 'Hi Sherly'.split('')
+const title = document.querySelector('.title');
+const text = 'The Land of Enchantment.'.split('');
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+    title.innerHTML += `<span>${text[index]}</span>`;
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 20px;'></span>`;
   }
 }
 
