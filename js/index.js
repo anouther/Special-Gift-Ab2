3565112fc1f857e8a270832ea0fed2563c52267c
anouther <155570107+anouther@users.noreply.github.com>
2025-02-13 10:46:52 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title');
-const text = 'The Land of Enchantment.'.split('');
+const text = 'The Land of    Enchantment.'.split('');
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}</span>`;
