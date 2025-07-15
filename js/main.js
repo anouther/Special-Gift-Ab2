5bf06f8a75fdccaa117ab1da5c0dee422fe17682
anouther <155570107+anouther@users.noreply.github.com>
2025-07-15 11:26:33 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Stumble & Blossom').split('');
+        const titles = ('Happy 3rd Mensiversary, Abang.').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "A theistic orchard blooms unfurled within the shadowed alcoves of my mind the moment our kismet encounter. Tendrils of serenity threading into the marrow of my being and transmuting desolation into a wild bloom of piety. I shall cradle you within the refuge of my arms, for you're more sacred than a plumule relinquishing its last daffodil to the lull of dusk.";
+            subtitle.innerText = "Thank you for walking this far with me—through every season, every emotion, and every version of myself. Let’s walking together side by side through every month ahead, hand in hand, coloring our journey with love, laughter, and dreams. I love you, Abang. I adore you beyond what words can describe—more than the stars in the sky or the beauty this world can offer. You are my favorite part of every day. ";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
