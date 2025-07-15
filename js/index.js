cb80386e204d1ae9a5a57bdc7b044c75254c3497
anouther <155570107+anouther@users.noreply.github.com>
2025-07-15 11:25:58 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title');
-const text = 'The Land of Enchantment.'.split('');
+const text = 'Hi Batman'.split('');
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}</span>`;
