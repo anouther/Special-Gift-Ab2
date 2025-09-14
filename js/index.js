8442ceb6d285db9feabfce8040d6cf646130cda4
anouther <155570107+anouther@users.noreply.github.com>
2025-09-14 22:17:01 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const title = document.querySelector('.title');
-const text = 'haii opall'.split('');
+const title = document.querySelector('.title')
+const text = 'Hi Opal'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}</span>`;
+    title.innerHTML += `<span>${text[index]}<span/>`
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'></span>`;
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
   }
 }
 
